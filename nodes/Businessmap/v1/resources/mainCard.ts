5dd5760738bae2280bcb7a5083cee6feaee508a6
Erlantz Carrasson <erlantz@businessmap.io>
2026-05-11 16:02:53 +0200

    fix: Archived cards on GET cards by single ID.
    card: https://rock.kanbanize.com/ctrl_board/782/cards/981936
    
    On branch master

--- nodes/Businessmap/v1/resources/mainCard.ts
+++ nodes/Businessmap/v1/resources/mainCard.ts
@@ -1411,33 +1411,18 @@ export const mainCardHandlers: IResourceHandler = {
 			throw new NodeOperationError(this.getNode(), `Card ID must be a positive number`, {level: 'warning',});
 		}
 
-		const qs: IDataObject = {};
-		qs.card_ids = cardIdentifier;
-
-		const { fields, expand } = getSelectedFields.call(this, itemIndex);
-		if (fields !== '') {
-			qs.fields = fields;
-		}
-		if (expand !== '') {
-			qs.expand = expand;
-		}
-
 		const response = await businessmapApiRequest.call(
 			this,
 			'GET',
-			`/cards/`,
+			`/cards/${cardIdentifier}`,
 			undefined,
-			qs,
 		);
 
-		// pull out the parsed JSON (response.data.data) and then its data array
-		// First `data` is from businessmapApiRequest, second is from Businessmap main `data` and third `data` are the actual cards
-		const items = response.data?.data?.data;
-		if (Array.isArray(items) && items.length > 0) {
-			return formatCardOutput.call(this, items, itemIndex);
+		const item = response.data?.data;
+		if (item && typeof item === 'object' && !Array.isArray(item)) {
+			return formatCardOutput.call(this, [item], itemIndex);
 		}
 
-		// 4) nothing found → return an empty object
 		return {status: 'Card not found'};
 	},
 
